4b5f5faa230df2b665bcd9c3bace42f01065b516
Kevin Upton <k_upton@live.com>
2017-11-30 23:58:19 +1000

    formatted the config

--- src/config/config.php
+++ src/config/config.php
@@ -6,23 +6,22 @@ use Illuminate\Validation\ValidationException;
 use Kevupton\LaravelJsonResponse\JsonResponse;
 
 return [
-  'exceptions' => [
-      /**
-       * Show model not found when receiving this error
-       */
-      ModelNotFoundException::class => [
-          'error' => 'Model not found',
-          'setStatusCode' => Response::HTTP_NOT_FOUND
-      ],
+    'exceptions' => [
+        /**
+         * Show model not found when receiving this error
+         */
+        ModelNotFoundException::class => [
+            'error' => 'Model not found',
+            'setStatusCode' => Response::HTTP_NOT_FOUND
+        ],
 
-      /**
-       * Add all the errors from the validation and continue
-       */
-      ValidationException::class =>  function (ValidationException $e, JsonResponse $json)
-      {
-          $json
-              ->mergeErrors($e->errors())
-              ->setStatusCode(Response::HTTP_UNPROCESSABLE_ENTITY);
-      }
-  ]
+        /**
+         * Add all the errors from the validation and continue
+         */
+        ValidationException::class => function (ValidationException $e, JsonResponse $json) {
+            $json
+                ->mergeErrors($e->errors())
+                ->setStatusCode(Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+    ]
 ];
